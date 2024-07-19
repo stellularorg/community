ba9a8c0710aa9acd569a6417a5b319916ca13ca3
hkauso <me@hkau.org>
2024-07-18 20:22:40 -0400

    Delete ubuntu/home/ubuntu/scripts directory

--- ubuntu/home/ubuntu/scripts/run-crangon.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ubuntu/crangon
-sudo pkill -e crangon
-./target/release/crangon --port 8080 --staic-dir "/home/ubuntu/crangon/static"
--- ubuntu/home/ubuntu/scripts/run-guppy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ubuntu/guppy
-sudo pkill -e guppy
-./target/release/guppy --port 8081 --staic-dir "/home/ubuntu/guppy/static"
